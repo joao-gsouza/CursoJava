d462580ed53a1fb83fa26605d9ef44b08bae97cf
joao guilherme <gui_joao6@outlook.com>
2020-04-08 12:59:24 -0300

    Aula e exercicio 14

---
+++ Vetor01/src/vetor01/Vetor01.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vetor01;
+
+/**
+ *
+ * @author gui_j
+ */
+public class Vetor01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n[] = {3,4,5,6,7,9};
+        
+        for (int c = 0; c < n.length; c++) {
+            System.out.println("Na posicao " + c + " temos o valor: " +n[c]);
+        }
+        
+    }
+    
+}
